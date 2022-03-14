455f3f4fe7f7d4cccf508a75ecf723272d132c21
Captain Sir <jakeovski@gmail.com>
2022-03-14 01:35:12 +0000

    Ver 1.0.0-alpha5 # Fixed server-side passwordReset

--- pages/passwordReset/index.js
+++ pages/passwordReset/index.js
@@ -2,7 +2,11 @@ import React, {useState} from 'react';
 import {useRouter} from "next/router";
 import {Alert, Button, Container, Grid, LinearProgress, Paper, Typography} from "@mui/material";
 import Input from "../../helper/Input";
-import check from "../api/email/check";
+import Connection from "../../config/dbConnection";
+import User from "../../models/User";
+import ResetToken from "../../models/ResetToken";
+import {compare} from "bcrypt";
+import {DateTime} from "luxon";
 
 /**
  * Password Reset Page
@@ -12,7 +16,6 @@ import check from "../api/email/check";
  */
 const PasswordReset = ({data}) => {
     const router = useRouter();
-    console.log(data);
     const [formData, setFormData] = useState({
         password: '',
         confirmPassword: '',
@@ -167,10 +170,85 @@ const PasswordReset = ({data}) => {
 export async function getServerSideProps({query}) {
     const {user, token} = query;
 
-    const data = await check(user, token);
+    // const data = await check(user, token);
+    await Connection();
+    const foundUser = await User.findOne({
+        email: user
+    });
+
+    //If no user found return an error
+    if (!foundUser) {
+        console.log("No User");
+        return {
+            props: {
+                data: {
+                    error: true,
+                    message: 'Invalid URL'
+                }
+            }
+        };
+    }
+
+    //Check whether the reset token exists in the db
+    const resetToken = await ResetToken.findOne({
+        userId: foundUser._id
+    });
+
+    //If no reset token return an error
+    if (!resetToken) {
+        console.log("No Reset Token");
+        console.log(resetToken);
+        return {
+            props: {
+                data: {
+                    error: true,
+                    message: 'Invalid URL'
+                }
+            }
+        };
+    }
+
+    //Check whether the hashes match between the tokens
+    const checkToken = await compare(token, resetToken.tokenHash);
+
+    //If they do not match return an error
+    if (!checkToken) {
+        console.log("Tokens do not match");
+        return {
+            props: {
+                data: {
+                    error: true,
+                    message: 'Invalid URL'
+                }
+            }
+        };
+    }
+
+    //Check if the token has expired
+    if (resetToken.expirationDate < DateTime.now()) {
+        //Delete the token
+        await ResetToken.deleteOne({
+            userId: resetToken.userId
+        });
+        //Return an appropriate error
+        return {
+            props: {
+                data: {
+                    error: true,
+                    message: 'Link Expired'
+                }
+            }
+        };
+    }
+
+    //If no errors, return the response
     return {
         props: {
-            data
+            data: {
+                error: false,
+                message: 'Provide new password',
+                id: JSON.parse(JSON.stringify(foundUser._id))
+            }
         }
     }
 }
